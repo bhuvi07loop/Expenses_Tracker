05f3c566c9f8fa1f5bd831f8d7b60928ceeef427
Bhuvanesh <sribhuvi6264@gmail.com>
2026-06-10 15:20:22 +0530

    fix logout

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("/")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # Google login URLs
     path("auth/", include("social_django.urls", namespace="social")),
+
+    # Logout URL
+    path("logout/", logout_view, name="logout"),
+
+    # Your app URLs
     path("", include("expenses.urls")),
 ]
