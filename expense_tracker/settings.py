9c07722f8de979600c51c934d5c6831e05f662be
Bhuvanesh <sribhuvi6264@gmail.com>
2026-06-10 14:45:05 +0530

    Add PostgreSQL

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -1,5 +1,5 @@
 import os
-from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -66,12 +66,12 @@ WSGI_APPLICATION = "expense_tracker.wsgi.application"
 
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "Asia/Kolkata"
